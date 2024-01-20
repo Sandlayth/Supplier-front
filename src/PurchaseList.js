575184038a75cf0e5e9498adc3b56ed5e2921bf9
Yassine Hadj messaoud <modepadu95@riseup.net>
2024-01-20 23:20:16 +0100

    Don't display user column if data don't exist

--- src/PurchaseList.js
+++ src/PurchaseList.js
@@ -1,10 +1,13 @@
 // src/PurchaseList.js
 import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Button } from 'react-bootstrap';
 import { authenticatedAxios } from './api';
+import PurchaseForm from './PurchaseForm';
 
 const PurchaseList = () => {
   const [purchases, setPurchases] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     const fetchPurchases = async () => {
@@ -16,12 +19,43 @@ const PurchaseList = () => {
       }
     };
 
+    const fetchLocations = async () => {
+      try {
+        const data = await authenticatedAxios('locations');
+        setLocations(data);
+      } catch (error) {
+        console.error('Error fetching locations:', error.message);
+      }
+    };
+
+
     fetchPurchases();
+    fetchLocations();
   }, []);
+
+  const handleCreatePurchase = async (newPurchase) => {
+    try {
+      // Send POST request to create a new purchase
+      console.log(newPurchase)
+      await authenticatedAxios('purchases', 'post', newPurchase);
+
+      // Fetch updated purchase list
+      const updatedPurchases = await authenticatedAxios('purchases');
+      setPurchases(updatedPurchases);
+
+      // Close the form modal
+      setShowForm(false);
+    } catch (error) {
+      console.error('Error creating purchase:', error.message);
+    }
+  }
 
   return (
     <div>
       <h2>Purchase List</h2>
+      <Button variant="primary" onClick={() => setShowForm(true)}>
+        Create
+      </Button>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -30,7 +64,7 @@ const PurchaseList = () => {
             <th>Total Price</th>
             <th>Fees</th>
             <th>Calculated Price</th>
-            <th>User</th>
+            {purchases[0] && purchases[0].userName && <th>User</th>}
             <th>Location</th>
           </tr>
         </thead>
@@ -42,12 +76,19 @@ const PurchaseList = () => {
               <td>{purchase.totalPrice}</td>
               <td>{purchase.fees}</td>
               <td>{purchase.totalPrice * (1 - purchase.fees)}</td>
-              <td>{purchase.user}</td>
-              <td>{purchase.location}</td>
+              {purchase.userName && <td>{purchase.userName}</td>}
+              <td>{purchase.locationName} ({purchase.supplierName})</td>
             </tr>
           ))}
         </tbody>
       </Table>
+      {/* Modal for creating a new purchase */}
+      <PurchaseForm
+        show={showForm}
+        handleClose={() => setShowForm(false)}
+        handleCreate={handleCreatePurchase}
+        locations={locations} // Pass locations to the form
+      />
     </div>
   );
 };
